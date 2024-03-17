fc8268333bf5dcb277de3a827e4d7a62b521744e
Jose Manuel Cuello Royo <56176862+Josemcr1134@users.noreply.github.com>
2024-03-16 20:21:58 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  base_url: 'https://netcareapi.ttncompany.com/api',
+  base_url: 'https://pygfurughz.hidoctor.app',
   pub_wompi_test: 'pub_test_0lFRLbzcrYKgi6AyL3qLBThez7I6KRki',
   redirect_url: 'https://pre-affiliates.netcare.life/Home/Dates/MyDates',
 };
