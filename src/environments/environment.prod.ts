bf468e59fcc3f8ab48d00893dff0eb22fa5ea084
Jose Manuel Cuello Royo <56176862+Josemcr1134@users.noreply.github.com>
2024-03-16 20:47:06 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  base_url: 'https://pygfurughz.hidoctor.app',
+  base_url: 'https://pygfurughz.hidoctor.app/api',
   pub_wompi_test: 'pub_test_0lFRLbzcrYKgi6AyL3qLBThez7I6KRki',
   redirect_url: 'https://pre-affiliates.netcare.life/Home/Dates/MyDates',
 };
